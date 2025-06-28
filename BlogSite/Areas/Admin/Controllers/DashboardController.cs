009ad788015f1868568e00af2b5644dfcbef5860
Cavid112233 <cavidsly@gmail.com.com>
2025-06-29 00:54:57 +0400

    .....

--- BlogSite/Areas/Admin/Controllers/DashboardController.cs
+++ BlogSite/Areas/Admin/Controllers/DashboardController.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BlogSite.Areas.Admin.ViewModels;
+using BlogSite.Data.DAL;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogSite.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize] 
+    [Authorize]
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var messages = _context.ContactMessages.OrderByDescending(m => m.CreatedAt).Take(5).ToList();
+            var portfolios = _context.Portfolio.OrderByDescending(p => p.Id).Take(5).ToList();
+
+            var vm = new DashboardVM
+            {
+                ContactMessages = messages,
+                Portfolios = portfolios
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ BlogSite/Areas/Admin/ViewModels/DashboardVM.cs
@@ -0,0 +1,11 @@
+﻿using BlogSite.Core.Models;
+
+namespace BlogSite.Areas.Admin.ViewModels
+{
+    public class DashboardVM
+    {
+        public IEnumerable<ContactMessage> ContactMessages { get; set; }
+        public List<Portfolio> Portfolios { get; set; }
+    }
+
+}
